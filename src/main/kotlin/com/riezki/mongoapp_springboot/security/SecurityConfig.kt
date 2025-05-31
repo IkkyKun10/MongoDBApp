d623c0eee034ebf9c09ac1c8fc1ce30a689870de
riezkymr <riezkymaisyar10@gmail.com>
2025-05-31 21:53:21 +0700

    fixing config

--- src/main/kotlin/com/riezki/mongoapp_springboot/security/SecurityConfig.kt
+++ src/main/kotlin/com/riezki/mongoapp_springboot/security/SecurityConfig.kt
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.HttpStatusEntryPoint
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 class SecurityConfig(
@@ -35,7 +36,7 @@ class SecurityConfig(
                     HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)
                 )
             }
-            .addFilterBefore(jwtAuthFilter, JwtAuthFilter::class.java)
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter::class.java)
             .build()
     }
 
